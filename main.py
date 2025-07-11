23f42abe58d18e0a33f82646e6ce100a6222f3c8
MrZomka <zomka@fard.ing>
2025-07-11 22:45:31 +0300

    Fix #7

--- main.py
+++ main.py
@@ -309,7 +309,7 @@ class LaunchEnvWindow(QDialog):
             logging.info("Launching vanilla game.")
             if current_launch_options != "":
                 logging.info(f"Launch options: {current_launch_options}")
-            subprocess.Popen([sys.argv[-1], current_launch_options], cwd=os.path.dirname(sys.argv[-1]), start_new_session=True)
+            subprocess.Popen([get_default_game_executable(self.game), current_launch_options], cwd=os.path.dirname(sys.argv[-1]), start_new_session=True)
         else:
             env_launcher = os.path.join(config_data.get("env_location"), self.game, env_id, get_default_game_executable(self.game))
             if not os.path.exists(env_launcher):
